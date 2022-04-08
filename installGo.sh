64943a0cb1ddde087c4603b9441d559756745c09
404a10 <55993927+404a10@users.noreply.github.com>
2022-04-08 11:28:07 +0200

    Delete installGo.sh

--- installGo.sh
+++
@@ -1,5 +0,0 @@
-curl -s -o /tmp/go.tar.gz "https://golang.google.cn/dl/go1.18.linux-amd64.tar.gz"
-sudo tar -C /usr/local /tmp/go.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-echo "Check go installation:"
-go version
